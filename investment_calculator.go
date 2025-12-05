4b5a1333e13fb20c537a8044ebb464daebade55c
Sleman Khadoor <khadoor.sleman@gmail.com>
2025-12-05 06:24:33 +0400

    alternative variable declaration

--- investment_calculator.go
+++ investment_calculator.go
@@ -6,11 +6,7 @@ import (
 )
 
 func main() {
-	// we could uses explicit type assignment insted of doing the type conversion later
-	// var investAmount float64 = 1000
-	// internaly 1000 will be stored as 1000.0
-	// and using that we can get rid of the type conversions
-	//------------------------------------------------------
+
 	/*
 		var investAmount = 1000
 		var expectedReturnRate = 5.5
@@ -18,9 +14,26 @@ func main() {
 		var futureValue = float64(investAmount) * math.Pow((1+expectedReturnRate/100), float64(years))
 		fmt.Println(futureValue)
 	*/
-	var investAmount float64 = 1000
-	var expectedReturnRate = 5.5
-	var years float64 = 10
-	var futureValue = investAmount * math.Pow((1+expectedReturnRate/100), years)
+	//------------------------------------------------------
+	// we could uses explicit type assignment insted of doing the type conversion later
+	// var investAmount float64 = 1000
+	// internaly 1000 will be stored as 1000.0
+	// and using that we can get rid of the type conversions
+	//------------------------------------------------------
+	/*
+		var investAmount float64 = 1000
+		var expectedReturnRate = 5.5
+		var years float64 = 10
+		var futureValue = investAmount * math.Pow((1+expectedReturnRate/100), years)
+		fmt.Println(futureValue)
+	*/
+	//------------------------------------------------------
+	// Alternative variable declaration
+	// Here we use := to declare and assign a value to the variable and we can't use explicit type here
+	// we can declare multiple varables on the same line and they don't need to be of the same type
+	var investAmount, years float64 = 1000, 10
+	expectedReturnRate := 5.5
+	futureValue := investAmount * math.Pow((1+expectedReturnRate/100), years)
 	fmt.Println(futureValue)
+
 }
